1f8d9a75399de7c7cb17b9933d52518998a12686
Alirio Rosales <alirio157@gmail.com>
2023-08-10 23:19:46 -0600

    practica 2

---
+++ Practica2/Credito.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Credito{
+
+    public static void main(String[] args) {
+//definimos variables
+    double prestamo,cuota,pago ;
+    int descuento;
+    String periodo,mensual, trimestral, semetral, anual;
+    Scanner obj = new Scanner(System.in);
+// pide monto credito
+    System.out.print("Ingrese valor de prestamo: ");
+    prestamo = obj.nextDouble(); 
+    System.out.print("Ingrese tipo de pago (mensual, trimestral, semetral, anual): ");
+    periodo = obj.nextLine();
+//calcula el rango en el que se encuentra el credito
+if (prestamo>=2000 && prestamo <=5000) {
+    if(periodo==mensual){
+        
+    }
+    return double total=
+    }
+if(prestamo>=5001 && prestamo <=1000){
+        return
+    }
+if(prestamo>1000) {
+
+        }
+}
+
+}
