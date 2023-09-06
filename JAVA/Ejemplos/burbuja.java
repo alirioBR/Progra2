7b000b5e9cdd92e665ea9421e154db3e0d6a11cc
Alirio B. Rosales <alrosales@osinearshore.com>
2023-09-06 16:30:43 -0600

    burbuja

---
+++ JAVA/Ejemplos/burbuja.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class burbuja;
+
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int aux = 0;
+    int elementos *5;
+    int nums[] = new int[elementos];
+    for(int i=0;i<elementos;i++){
+        nums[i] = sc.nextInt();
+}
+    for(int j=0;j<elementos-1;j++){
+        for(int k=0;k<elementos-1;k++){
+            if(nums[k] > nums[k+1]){
+                aux = nums[k];
+                nums[k] = nums[k+1];
+                nums[k+1] = aux;
+            }
+
+    if(j==3){
+
+
+        
+    }
+        }
+    }
+}
+
