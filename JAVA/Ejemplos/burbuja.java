7c2d76ab98a11dd16111369324e406abd534cbfb
Alirio B. Rosales <alrosales@osinearshore.com>
2023-09-08 18:38:11 -0600

    burbuja

--- JAVA/Ejemplos/burbuja.java
+++ JAVA/Ejemplos/burbuja.java
@@ -6,17 +6,18 @@ public class burbuja{
 public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int aux = 0;
+    boolean orden= true;
     System.out.println("ingrese la cantidad de elementos:");
     int elementos = sc.nextInt();
-    //System.out.println("Ordenar de forma ascendente SI/NO?:");
-    //boolean (orden==true) = sc.nextBoolean();
-
-    
-    try {
-    int nums[] = new int[elementos];
-    System.out.println("ingrese elementos:");
-    for(int i=0;i<elementos;i++){
-        nums[i] = sc.nextInt();
+    System.out.println("Ordenar de forma ascendente SI/NO?:");
+    boolean orden = sc.nextBoolean();
+  
+    if(orden)
+        try {
+        int nums[] = new int[elementos];
+        System.out.println("ingrese elementos:");
+            for(int i=0;i<elementos;i++){
+            nums[i] = sc.nextInt();
 }
     for(int j=0;j<elementos-1;j++){
         for(int k=0;k<elementos-1;k++){
@@ -31,6 +32,13 @@ public static void main(String[] args) {
     System.out.println("Lista de elementos ordenados: "); 
         for(var i=0; i<elementos; i++) 
         {System.out.println(nums[i]);}
+    }
+    else{
+        System.out.println("Lista de elementos ordenados: "); 
+        for(var i=0; i>elementos; i++) 
+        {System.out.println(nums[i]);}
+
+
     }
     catch (InputMismatchException ex) {
         System.out.println("Debe ingresar obligatoriamente un número entero.");
