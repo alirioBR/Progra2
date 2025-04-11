896945eacce7eb5359f3415b8b04bf709300f064
Alirio Rosales <alirio157@gmail.com>
2025-04-10 21:28:10 -0600

    mvc

---
+++ proymvc/proymvc/src/main/java/com/claseafe/proymvc/ProymvcApplication.java
@@ -0,0 +1,13 @@
+package com.claseafe.proymvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProymvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProymvcApplication.class, args);
+	}
+
+}
